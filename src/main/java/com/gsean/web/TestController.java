6c344bb3fb893861ad96db0f882469860544e2ba
guoc <770292311@qq.com>
2020-08-12 14:47:54 +0800

    ssss

--- src/main/java/com/gsean/web/TestController.java
+++ src/main/java/com/gsean/web/TestController.java
@@ -23,11 +23,6 @@ public class TestController {
 
 
     public static void main(String[] args) {
-        System.out.println("hhhhs");
-        System.out.println("sss");
-        System.out.println("sss");
-        System.out.println("sss");
-        System.out.println("sss");
-        System.out.println("sss");
+        System.out.println("sssss");
     }
 }
