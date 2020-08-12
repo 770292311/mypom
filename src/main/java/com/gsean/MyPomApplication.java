5134d4e822d9206cea5c82387c0b1fa4d7750a04
guoc <770292311@qq.com>
2020-08-12 09:34:51 +0800

    init

---
+++ src/main/java/com/gsean/MyPomApplication.java
@@ -0,0 +1,19 @@
+package com.gsean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ProjectName：mypom
+ * ClassName：MyPomApplication
+ * Description：TODO
+ * Date：2020/8/11 15:14
+ * Author：GSean
+ * Version: 1.0
+ */
+@SpringBootApplication
+public class MyPomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyPomApplication.class,args);
+    }
+}
---
+++ src/main/java/com/gsean/web/TestController.java
@@ -0,0 +1,23 @@
+package com.gsean.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ProjectName：mypom
+ * ClassName：TestController
+ * Description：TODO
+ * Date：2020/8/11 15:13
+ * Author：GSean
+ * Version: 1.0
+ */
+@RestController
+public class TestController {
+    @Value("${test.str}")
+    private String strValue;
+    @GetMapping(value = "/test")
+    public String test(){
+        return "hello pom:"+strValue;
+    }
+}
